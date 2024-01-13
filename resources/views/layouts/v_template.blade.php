55608728a713ed4f58fa1dd1dd8af1246c5cbf62
Akbar <muhammad.akbar@arkamaya.co.id>
2024-01-13 22:15:34 +0700

    s

--- resources/views/layouts/v_template.blade.php
+++ resources/views/layouts/v_template.blade.php
@@ -47,7 +47,7 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <h1 class="logo"><a href="index.html">Karir</a></h1>
+      <h1 class="logo"><a href="index.html">{{ env('APP_NAME', 'Apps'); }}</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo"><img src="{{asset('template')}}/img/logo.png" alt="" class="img-fluid"></a>-->
 
---
+++ resources/views/v_contact.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.v_template')
+
+@section('title', 'Test Information')
+
+@section('style')
+
+@endsection
+
+@section('script')
+
+@endsection
+
+@section('content')
+
+
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact section-bg">
+  <div class="container mt-5" data-aos="fade-up">
+
+    <div class="section-title">
+      <h2>Contact</h2>
+      <p>Ada pertanyaan? Hubungi kami!</p>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-6">
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="info-box">
+              <i class="bx bx-map"></i>
+              <h3>Our Address</h3>
+              <p>Jalan Karir No.123 Blok 09, RT 02/01, Kecamatan Sukamenah, Kota Bandung, Jawa barat 40156</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="info-box mt-4">
+              <i class="bx bx-envelope"></i>
+              <h3>Email Us</h3>
+              <p>tanya@contoh.com<br>cs@contoh.com</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="info-box mt-4">
+              <i class="bx bx-phone-call"></i>
+              <h3>Call Us</h3>
+              <p>+xxxxxxxxx<br>+xxxxxxxxx</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-lg-6 mt-4 mt-md-0">
+        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+          <div class="row">
+            <div class="col-md-6 form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+            </div>
+            <div class="col-md-6 form-group mt-3 mt-md-0">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+            </div>
+          </div>
+          <div class="form-group mt-3">
+            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+          </div>
+          <div class="form-group mt-3">
+            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+          </div>
+          <div class="my-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your message has been sent. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Send Message</button></div>
+        </form>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Contact Section -->
+
+
+@endsection
